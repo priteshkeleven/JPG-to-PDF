631f4bab6827d2f5cca3de9253e91f4a5a6aa6c3
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2025-01-24 18:25:13 +0530

    Added Google Analytics Code

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-PN4GR86V6V" />
     </html>
   );
 }
